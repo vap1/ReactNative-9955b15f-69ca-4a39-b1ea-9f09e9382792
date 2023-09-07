a1a9e824af5bc661bd862c09b7f57464c93e44c0
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:23:26 +0530

    First Commit

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Logging in...');
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Perform login API call here
+
+    // Example API call using fetch:
+    // fetch('/api/login', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ email, password }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log('Login response:', data);
+    //     // Handle login response here
+    //   })
+    //   .catch((error) => {
+    //     console.error('Login error:', error);
+    //     // Handle login error here
+    //   });
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default LoginScreen;
