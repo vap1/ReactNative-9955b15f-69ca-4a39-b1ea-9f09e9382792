d8420b724067162364719c34ddf8875a59bfb580
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:31:55 +0530

    First Commit

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { RegistrationScreen } from '../screens/RegistrationScreen';
-import { LoginScreen } from '../screens/LoginScreen';
-import { ProfileScreen } from '../screens/ProfileScreen';
+import RegistrationScreen from '../screens/RegistrationScreen';
+import LoginScreen from '../screens/LoginScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
