c40c57d67f4cf1fa914d75be793de26f2ec1cad2
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:31:56 +0530

    First Commit

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -7,29 +7,29 @@ const LoginScreen = () => {
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    console.log('Logging in...');
+    console.log('Step 1: Logging in...');
     console.log('Email:', email);
     console.log('Password:', password);
 
     // Perform login API call here
 
     // Example API call using fetch:
-    // fetch('/api/login', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ email, password }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log('Login response:', data);
-    //     // Handle login response here
-    //   })
-    //   .catch((error) => {
-    //     console.error('Login error:', error);
-    //     // Handle login error here
-    //   });
+    fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Step 2: Login response:', data);
+        // Handle login response here
+      })
+      .catch((error) => {
+        console.error('Login error:', error);
+        // Handle login error here
+      });
   };
 
   return (
