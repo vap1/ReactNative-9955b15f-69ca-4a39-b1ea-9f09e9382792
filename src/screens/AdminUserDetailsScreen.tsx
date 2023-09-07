9639780f5f6024c37871c28d6d605bfd72f08123
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:23:22 +0530

    First Commit

---
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -0,0 +1,33 @@
+
+import React, { useEffect, useContext } from 'react';
+import { View, Text } from 'react-native';
+import { AdminUserDetailsContext } from '../contexts/AdminUserDetailsContext';
+import { User } from '../types/Types';
+
+const AdminUserDetailsScreen: React.FC = () => {
+  const { users, getUsers } = useContext(AdminUserDetailsContext);
+
+  useEffect(() => {
+    console.log('Fetching admin user details...');
+    getUsers();
+  }, []);
+
+  console.log('Admin User Details:', users);
+
+  return (
+    <View>
+      <Text>Admin User Details</Text>
+      {users.map((user: User) => (
+        <View key={user.email}>
+          <Text>Name: {user.name}</Text>
+          <Text>Email: {user.email}</Text>
+          <Text>Contact Info: {user.contactInfo}</Text>
+          <Text>Address: {user.address}</Text>
+          <Text>Profile Picture: {user.profilePicture}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default AdminUserDetailsScreen;
