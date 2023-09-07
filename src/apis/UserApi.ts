b689c6d042046d39266677b9273bd3e5eee3dc04
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:49:38 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -1,5 +1,5 @@
 
-import { UserRegistrationRequest, UserRegistrationResponse, UserLoginRequest, UserLoginResponse } from '../types/Types';
+import { UserRegistrationRequest, UserRegistrationResponse, UserLoginRequest, UserLoginResponse, UserProfileRequest, UserProfileResponse } from '../types/Types';
 
 export const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
   try {
@@ -39,4 +39,32 @@ export const loginUser = async (request: UserLoginRequest): Promise<UserLoginRes
     console.error('Error occurred while logging in:', error);
     throw error;
   }
+};
+
+export const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
+  try {
+    console.log('Sending GET request to retrieve user profile...');
+    console.log('Request:', request);
+
+    // Simulating API call and generating random data
+    const userProfile = generateRandomUserProfile();
+
+    console.log('User profile retrieved successfully:', userProfile);
+    return { user: userProfile };
+  } catch (error) {
+    console.error('Failed to retrieve user profile:', error);
+    throw error;
+  }
+};
+
+// Helper function to generate random user profile data
+const generateRandomUserProfile = (): UserProfileResponse['user'] => {
+  // Generate random user profile data here
+  return {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    contactInfo: '123-456-7890',
+    address: '123 Main St',
+    profilePicture: 'https://example.com/profile.jpg',
+  };
 };
