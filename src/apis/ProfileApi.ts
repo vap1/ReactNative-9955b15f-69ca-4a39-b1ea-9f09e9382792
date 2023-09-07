47d45aa89335f14a6e88ae4da5a7c797df089e98
vap1 <vaibhavvasant1705@gmail.com>
2023-09-07 19:31:59 +0530

    First Commit

--- src/apis/ProfileApi.ts
+++ src/apis/ProfileApi.ts
@@ -3,16 +3,13 @@ import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, User
 
 export const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
   try {
-    console.log('Sending GET request to retrieve user profile...');
-    // Replace the following code with the actual API call to retrieve user profile
-    const userProfile = {
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      contactInfo: '123-456-7890',
-      address: '123 Main St',
-      profilePicture: 'https://example.com/profile.jpg',
-    };
-    console.log('User profile retrieved successfully:', userProfile);
+    console.log('Step 1: Sending GET request to retrieve user profile...');
+    console.log('Request:', request);
+
+    // Simulating API call and generating random data
+    const userProfile = generateRandomUserProfile();
+
+    console.log('Step 2: User profile retrieved successfully:', userProfile);
     return { user: userProfile };
   } catch (error) {
     console.error('Failed to retrieve user profile:', error);
@@ -22,18 +19,28 @@ export const getUserProfile = async (request: UserProfileRequest): Promise<UserP
 
 export const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
   try {
-    console.log('Sending PUT request to update user profile...');
-    // Replace the following code with the actual API call to update user profile
-    const updatedUserProfile = {
-      name: request.name || 'John Doe',
-      contactInfo: request.contactInfo || '123-456-7890',
-      address: request.address || '123 Main St',
-      profilePicture: request.profilePicture || 'https://example.com/profile.jpg',
-    };
-    console.log('User profile updated successfully:', updatedUserProfile);
+    console.log('Step 1: Sending PUT request to update user profile...');
+    console.log('Request:', request);
+
+    // Simulating API call and generating random data
+    const updatedUserProfile = generateRandomUserProfile();
+
+    console.log('Step 2: User profile updated successfully:', updatedUserProfile);
     return { success: true, message: 'User profile updated successfully' };
   } catch (error) {
     console.error('Failed to update user profile:', error);
     throw error;
   }
+};
+
+// Helper function to generate random user profile data
+const generateRandomUserProfile = (): User => {
+  // Generate random user profile data here
+  return {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    contactInfo: '123-456-7890',
+    address: '123 Main St',
+    profilePicture: 'https://example.com/profile.jpg',
+  };
 };
